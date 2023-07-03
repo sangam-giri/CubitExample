298f5f1392776e3f3cb8fd906d6c36c56b0eb73c
Sangam Giri <girisangam00@gmail.com>
2023-07-03 17:55:50 +0545

    color changer cubit

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -3,8 +3,21 @@ import 'package:cubit_test/controller/cubit/test_cubit/test_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 5), () {
+      BlocProvider.of<TestCubit>(context).loadedState();
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,25 +27,25 @@ class HomePage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              ElevatedButton(
-                  onPressed: () {
-                    BlocProvider.of<TestCubit>(context).initialState();
-                  },
-                  child: Text("Emit Initial")),
-              ElevatedButton(
-                  onPressed: () {
-                    BlocProvider.of<TestCubit>(context).loadedState();
-                  },
-                  child: Text("Emit Loaded")),
-              ElevatedButton(
-                  onPressed: () {
-                    BlocProvider.of<TestCubit>(context).errorState();
-                  },
-                  child: Text("Emit Error")),
+              // ElevatedButton(
+              //     onPressed: () {
+              //       BlocProvider.of<TestCubit>(context).initialState();
+              //     },
+              //     child: Text("Emit Initial")),
+              // ElevatedButton(
+              //     onPressed: () {
+              //       BlocProvider.of<TestCubit>(context).loadedState();
+              //     },
+              //     child: Text("Emit Loaded")),
+              // ElevatedButton(
+              //     onPressed: () {
+              //       BlocProvider.of<TestCubit>(context).errorState();
+              //     },
+              //     child: Text("Emit Error")),
               BlocBuilder<TestCubit, TestState>(
                 builder: (context, state) {
                   if (state is TestInitialState) {
-                    return Text("Initial State");
+                    return CircularProgressIndicator.adaptive();
                   }
                   if (state is TestErrorState) {
                     return Text("Error State");
